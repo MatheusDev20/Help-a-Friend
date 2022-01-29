5181e94ea0e18ff154293d0c8018ae8c24e9ac05
Matheus <matheusdev20@gmail.com>
2022-01-29 10:30:52 -0300

    Adding env variables for Entites and Migrations path

--- ormconfig.js
+++ ormconfig.js
@@ -1,19 +1,19 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['src/infra/db/postgres/entities/*.js'],
+  entities: [process.env.ENTITIES_PATH],
   migrationsTableName: 'haf_migrations',
   cli: {
     migrationsDir: 'src/infra/db/postgres/migrations',
   },
   migrations: [
-    'src/infra/migrations/typeorm/migrations/*.js',
+    process.env.MIGRATIONS_PATH,
   ],
-  ssl: true,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  }
+  // ssl: true,
+  // extra: {
+  //   ssl: {
+  //     rejectUnauthorized: false,
+  //   },
+  // }
 }
 
