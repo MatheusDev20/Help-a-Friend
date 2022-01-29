36ec3d5e858ca96a1a778b449e51e2a199b7f2c6
Matheus <matheusdev20@gmail.com>
2022-01-29 10:20:36 -0300

    Ajuste no ormconfig.json para as entidades apontarem para .js

--- ormconfig.js
+++ ormconfig.js
@@ -1,19 +1,19 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['src/infra/db/postgres/entities/*.ts'],
+  entities: ['src/infra/db/postgres/entities/*.js'],
   migrationsTableName: 'haf_migrations',
   cli: {
     migrationsDir: 'src/infra/db/postgres/migrations',
   },
   migrations: [
-    'src/infra/migrations/typeorm/migrations/*.ts',
+    'src/infra/migrations/typeorm/migrations/*.js',
   ],
-  ssl: true,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  }
+  // ssl: false,
+  // extra: {
+  //   ssl: {
+  //     rejectUnauthorized: true,
+  //   },
+  // }
 }
 
