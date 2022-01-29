036975424e872cc75c375ef685b52eeca304ff90
Matheus <matheusdev20@gmail.com>
2022-01-29 08:58:41 -0300

    Adicionando as configuracoes do heroku

---
+++ ormconfig.ts
@@ -0,0 +1,12 @@
+export default {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: ['src/infra/db/postgres/entities/*.ts'],
+  migrationsTableName: 'haf_migrations',
+  cli: {
+    migrationsDir: 'src/infra/db/postgres/migrations',
+  },
+  migrations: [
+    'src/infra/migrations/typeorm/migrations/*.ts',
+  ],
+};
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
 import app from './config/app';
+
+dotenv.config();
 
 app.listen(process.env.PORT, () => {
   console.log(`App Running on PORT: ${process.env.PORT}`);
