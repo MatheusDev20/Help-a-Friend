e09ec6fb4a9e4f881bacb593dc4a5eef8bb55dc2
MatheusDev20 <matheusdev20@gmail.com>
2023-01-29 15:50:37 -0300

    fix: cors errro

--- docs/deploy.md
+++ docs/deploy.md
@@ -6,3 +6,4 @@
     -> ssh matheus@204.48.26.219
     -> pull the last version of the origin github branch
     -> "pm2 list" to list the current node process running
+    -> pm2 start /dist/main/server.js to start the server.
--- src/main/config/app-middlewares.ts
+++ src/main/config/app-middlewares.ts
@@ -1,10 +1,10 @@
 import express, { Express } from 'express';
-
-import { enableCors, jsonParser } from '../middlewares';
+import cors from 'cors';
+import { jsonParser } from '../middlewares';
 import upload from '../../config/upload';
 
 export default (app: Express): void => {
   app.use('/files', express.static(upload.directory));
   app.use(jsonParser);
-  app.use(enableCors);
+  app.use(cors());
 };
