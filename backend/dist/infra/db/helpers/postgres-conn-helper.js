4792936a7616e8910c462fe48e65d4713c551a81
MatheusDev20 <matheusdev20@gmail.com>
2023-03-06 00:25:24 -0300

    new build

--- backend/dist/infra/db/helpers/postgres-conn-helper.js
+++ backend/dist/infra/db/helpers/postgres-conn-helper.js
@@ -7,6 +7,7 @@ const postgresConnection = new Promise((resolve, reject) => {
     try {
         console.log(process.env.DATABASE_URL);
         resolve((0, typeorm_1.createConnection)());
+        console.log('Just to see');
     }
     catch (error) {
         reject(error);
